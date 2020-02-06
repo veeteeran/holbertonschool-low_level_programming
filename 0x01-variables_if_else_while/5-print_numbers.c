03c049bd4d0f2bf02e2e245cc0a664d33cd0cf8c
Viet Tran <vietnamtran@gmail.com>
2020-02-06 22:27:30 +0000

    Print single digit base 10 numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,10 +2,19 @@
 /**
  * main - Entry point
  *
+ * Print all single digit base 10 numbers
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+
 	return (0);
 }
