ecc27f6b5a4cd6fa49fe142a066a58c14c66f201
Viet Tran <vietnamtran@gmail.com>
2020-02-06 22:42:57 +0000

    Print lowercase alphabet backwards using putchar

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,10 +2,19 @@
 /**
  * main - Entry point
  *
+ * Print lowercase alphabet in reverse
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	int i;
+	
+	for (i = 122; i > 97; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
 	return (0);
 }
