2f8c583db4f08ac8da65387af2ce5f7a820ec7e4
Viet Tran <vietnamtran@gmail.com>
2020-02-06 22:35:14 +0000

    Print single digit base 10 nums using putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,10 +2,19 @@
 /**
  * main - Entry point
  *
+ * Print single digit base 10 numbers using putchar
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
 	return (0);
 }
