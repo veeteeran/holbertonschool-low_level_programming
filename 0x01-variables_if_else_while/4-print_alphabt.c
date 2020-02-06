7bcfd8bfdc967fda8e9ec3ae23598470bab94456
Viet Tran <vietnamtran@gmail.com>
2020-02-06 22:21:16 +0000

    Print lowercase letters skip e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,5 +7,17 @@
  */
 int main(void)
 {
+	char c = 'a';
+	int i;
+	int length = 26;
+
+	for (i = 0; i < length; i++)
+	{
+		if (c != 'e' && c != 'q')
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+
 	return (0);
 }
