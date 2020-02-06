7148f1ea715e3ab929883a9e1b2efd54f2f8b9d4
Viet Tran <vietnamtran@gmail.com>
2020-02-06 22:58:09 +0000

    Print all base 16 numbers in lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,10 +2,24 @@
 /**
  * main - Entry point
  *
+ * Print all base 16 numbers in lower case
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	int i;
+	int j;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (j = 97; j < 103; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+
 	return (0);
 }
