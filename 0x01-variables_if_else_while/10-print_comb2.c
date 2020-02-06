f476bcd9c1b2bb63cf7495c514c2056aad9dfd39
Viet Tran <vietnamtran@gmail.com>
2020-02-06 23:54:42 +0000

    Print numbers 00 to 99 comma separated

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -2,10 +2,33 @@
 /**
  * main - Entry point
  *
+ * Print numbers 00-99 comma separated using putchar
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	int i;
+	int j;
+
+	for (i = 48; i < 58; i++)
+	{
+		j = 48;
+		while (j < 58)
+		{
+			putchar(i);
+			putchar(j);
+			if (i == 57 && j == 57)
+				break;
+			else
+			{
+				putchar(44);
+				putchar(32);
+			}
+			j++;
+		}
+	}
+	putchar('\n');
+
 	return (0);
 }
