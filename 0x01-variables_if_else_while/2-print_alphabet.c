17dfcb14deb860ea3d63c61c8ece44ef3e5142b7
Viet Tran <vietnamtran@gmail.com>
2020-02-06 19:27:46 +0000

    Prints lowercase alphabet using only putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,24 @@
+#include <string.h>
 #include <stdio.h>
 /**
  * main - Entry point
  *
+ * Print lower case alphabet using only putchar()
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	char alpha[27] = "abcdefghijklmnopqrstuvwxyz";
+	int len_alpha = strlen(alpha);
+	int i = 0;
+
+	while (i <= len_alpha)
+	{
+		putchar(alpha[i]);
+		i++;
+	}
+	putchar('\n');
+
 	return (0);
 }
