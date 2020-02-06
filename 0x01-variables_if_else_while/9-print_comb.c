667f6932436e722e01bed4b1caa03d5ecb6f8888
Viet Tran <vietnamtran@gmail.com>
2020-02-06 23:16:49 +0000

    Change condition fro ascii char to int

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,10 +2,24 @@
 /**
  * main - Entry point
  *
+ * Print all possible cominations of single digit numbers comma separated"
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+
 	return (0);
 }
