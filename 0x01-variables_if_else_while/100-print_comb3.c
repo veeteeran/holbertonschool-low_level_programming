29272c79155fa82a7ac0b87182847e2ede089f00
Viet Tran <vietnamtran@gmail.com>
2020-02-07 13:20:18 +0000

    Commit .c file instead of object code

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Print numbers 00-99 comma separated using putchar
+ * Do not print same combo of digits 01 and 10 are considered the same
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int zero, nine, comma, space;
+	int counter;
+	int i, j;
+
+	zero = 48;
+	nine = 57;
+	comma = 44;
+	space = 32;
+	counter = 1;
+	for (i = zero; i <= nine; i++)
+	{
+		j = zero;
+		if (i > zero)
+			j += counter;
+		while (j <= nine)
+		{
+			putchar(i);
+			putchar(j);
+			if (i == (nine - 1) && j == nine)
+				putchar('\n');
+			else
+			{
+				putchar(comma);
+				putchar(space);
+			}
+			j++;
+		}
+		counter ++;
+	}
+
+	return (0);
+}
