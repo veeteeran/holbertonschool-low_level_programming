c15025999cef9907d5a016c6de5893a9543af707
Viet Tran <vietnamtran@gmail.com>
2020-02-07 23:20:28 +0000

    Add newline after declaration

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
 /**
  * main - Entry point
  *
- *
+ * Print given str using write
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	printf("Len: %d\n", 59);
-	write(2, str, len);
+
+	write(2, str, 59);
 
 	return (1);
 }
