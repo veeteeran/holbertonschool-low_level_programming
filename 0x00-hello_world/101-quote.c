e66bf8686de8d6389c61706044452d2e3c6a3ae4
Viet Tran <vietnamtran@gmail.com>
2020-02-07 02:13:56 +0000

    Print given string to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 /**
  * main - Entry point
  *
- * 
  *
- * Return: Always 0 (Success) 
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
-{	
-	char str[59];
-	scanf("%s", str);
-	
-	puts(str);
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	int len = strlen(str);
+
+	write(2, str, len);
+	putchar('\n');
 
 	return (1);
 }
