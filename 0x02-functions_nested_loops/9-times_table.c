c32f34ecb8c1cdeae704cfa050214bcbbd853f34
Viet Tran <vietnamtran@gmail.com>
2020-02-11 05:17:29 +0000

    Print 9 times table, starting at 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,12 +1,40 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
- * main - Entry point
- *
- *
- *
- * Return: Always 0 (Success)
+ * times_table - prints 9 times table starting at 0
  */
-int main(void)
+void times_table(void)
 {
-	return (0);
+	/* Initialize variables*/
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{		
+			k = i * j;
+			if (j == 0)
+			{
+				_putchar('0');
+			}             
+			/* Values less than 10 can be added to ascii '0' */
+			else if (k < 10)
+			{
+				_putchar(',');
+                                _putchar(' ');
+                                _putchar(' ');
+				_putchar(k + '0');
+			}
+			/* Values greater than 10 digits printed separately */
+			else
+			{
+				_putchar(',');
+                                _putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}				
+		}
+		_putchar('\n');
+	}
 }
