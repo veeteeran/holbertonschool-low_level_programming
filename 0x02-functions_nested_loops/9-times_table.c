37390ac13e768f02def23c207d8c684bb2b0feb0
Viet Tran <vietnamtran@gmail.com>
2020-02-11 12:39:25 +0000

    Delete whitespaces, replace spaces with tabs

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,28 +12,28 @@ void times_table(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 9; j++)
-		{		
+		{
 			k = i * j;
 			if (j == 0)
 			{
 				_putchar('0');
-			}             
+			}
 			/* Values less than 10 can be added to ascii '0' */
 			else if (k < 10)
 			{
 				_putchar(',');
-                                _putchar(' ');
-                                _putchar(' ');
+				_putchar(' '):
+				_putchar(' ');
 				_putchar(k + '0');
 			}
 			/* Values greater than 10 digits printed separately */
 			else
 			{
 				_putchar(',');
-                                _putchar(' ');
+				_putchar(' ');
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
-			}				
+			}
 		}
 		_putchar('\n');
 	}
