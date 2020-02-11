19ce09011a35b21ff98d4e55f5564260405a2d04
Viet Tran <vietnamtran@gmail.com>
2020-02-11 15:51:49 +0000

    Create variable to hold digit, check condition beforeoutput

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
- * main - Entry point
+ * print_last_digit - print the last digit of n
+ * @n: the number to check
  *
- *
- *
- * Return: Always 0 (Success)
+ * Return: last digit of n
  */
-int main(void)
+int print_last_digit(int n)
 {
-	return (0);
+	int digit;
+	
+	if (n < 0)
+	{
+		n *= -1;
+	}	
+
+	digit = (n % 10);
+	_putchar(digit +'0');
+	
+	return (digit);
 }
