66fc0c3a895d15f0beb24f8f9e337e732afee9a1
Viet Tran <vietnamtran@gmail.com>
2020-02-11 00:17:53 +0000

    Print sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,12 +1,25 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
- * main - Entry point
+ * print_sign - Prints the sign of a number
+ * @n: The int to be checked
  *
- *
- *
- * Return: Always 0 (Success)
+ * Return: 1 if positive, 0 if zero, -1 if negative
  */
-int main(void)
+int print_sign(int n)
 {
-	return (0);
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
