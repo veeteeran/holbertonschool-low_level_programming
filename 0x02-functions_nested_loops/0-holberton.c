67e5ced53e5d97453bc4e75ae2b95d4171bcc874
Viet Tran <vietnamtran@gmail.com>
2020-02-11 00:14:17 +0000

    Print Holberton followed by a new line

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
  * main - Entry point
  *
- *
+ * Prints Holberton followed my a newline
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	char str[] = "Holberton";
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+
 	return (0);
 }
