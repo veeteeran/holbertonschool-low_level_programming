d85bbce06466b74f272e6ec21e885a16d8c6cfe3
Viet Tran <vietnamtran@gmail.com>
2020-02-13 21:17:11 +0000

    Rewrite condition for 0 and negative ints

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,17 +7,19 @@ void print_square(int size)
 {
 	int rows, cols;
 
-	if (size == 0)
-	{
-		_putchar('\n');
-	}
-
-	for (rows = 0; rows < size; rows++)
-	{
-		for (cols = 0; cols < size; cols++)
+	if (size > 0)
+	{	
+		for (rows = 0; rows < size; rows++)
 		{
-			_putchar('#');
+			for (cols = 0; cols < size; cols++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
