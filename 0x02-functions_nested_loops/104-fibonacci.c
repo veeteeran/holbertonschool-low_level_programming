a4c730f72935d4fcb83ec7bfd283ee15f31697b3
Viet Tran <vietnamtran@gmail.com>
2020-02-13 00:52:34 +0000

    Display first 98 fibonnaci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	double prev = 1;
+	double curr = 2;
+	double next;
+	int i;
+
+	printf("1, 2, ");
+	for (i = 0; i < 98; i++)
+	{
+		next = prev + curr;
+		printf("%.f", next);
+
+		if (i != 97)
+		{
+			printf(", ");
+		}
+		else
+		{
+			printf("\n");
+		}
+
+		prev = curr;
+		curr = next;
+	}
+	return (0);
+}
