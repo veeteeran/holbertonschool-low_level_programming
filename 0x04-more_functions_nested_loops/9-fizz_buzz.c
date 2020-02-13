cee5b2bcfdd06c505e22b122d299cad518e2fdb0
Viet Tran <vietnamtran@gmail.com>
2020-02-13 21:55:33 +0000

    First commit with tested code

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i == 1)
+		{
+			printf("%d ", i);
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" Fizz Buzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf(" Buzz");
+
+			if (i == 100)
+			{
+				printf("\n ");
+			}
+
+	
+		
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
+	return (0);
+}
