1ab140ab76fdedd565b0e9c36396acb0c582cf3c
Viet Tran <vietnamtran@gmail.com>
2020-02-13 19:16:27 +0000

    First commit with tested code

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * mul - multiplies two ints
+ * @a: first int to multiply
+ * @b: second int to multiply
+ *
+ * Return: the product of two ints
+ */
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
