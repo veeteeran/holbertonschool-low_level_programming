648c1058d7a087d5fdd04fae1766398c9c016038
Viet Tran <vietnamtran@gmail.com>
2020-02-13 21:11:20 +0000

    First commit with tested code

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_square - prints sqare followed by a newline
+ * @size: the size of the square
+ */
+void print_square(int size)
+{
+	int rows, cols;
+
+	if (size == 0)
+	{
+		_putchar('\n');
+	}
+
+	for (rows = 0; rows < size; rows++)
+	{
+		for (cols = 0; cols < size; cols++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
