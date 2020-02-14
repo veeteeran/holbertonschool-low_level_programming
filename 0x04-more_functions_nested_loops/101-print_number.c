78afc098c6e7050a99e2d6d50ff21a5adf0c6cfd
Viet Tran <vietnamtran@gmail.com>
2020-02-14 05:31:58 +0000

    First commit with tested code

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_number - print an int
+ * @n: the int to print
+ */
+void print_number(int n)
+{
+	int counter, digit;
+	int base_ten = 1000000000;
+	int skip_zero = 1;
+
+	counter = 0;
+	while (counter < 9)
+	{
+		if (n < 0)
+		{
+			n *= - 1;
+		}
+
+		digit = (n / base_ten) % 10;
+
+		if (digit == 0 && skip_zero)
+		{
+			base_ten /= 10;
+		}
+		else
+		{
+			skip_zero = 0;
+			_putchar(digit + '0');
+			base_ten /= 10;
+	
+		}
+		counter++;
+	}
+	_putchar(n % 10 + '0');
+}
