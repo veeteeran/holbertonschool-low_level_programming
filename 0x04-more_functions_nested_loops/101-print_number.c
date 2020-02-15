ffb89ffa643a387eef103de8e88f5337d6ab25c7
Viet Tran <vietnamtran@gmail.com>
2020-02-15 01:54:03 +0000

    Add conditions to output int min correctly

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * print_number - print an int
  * @n: the int to print
@@ -10,22 +9,28 @@ void print_number(int n)
 	int base_ten = 1000000000;
 	int skip_zero = 1;
 	int min  = -2147483648;
+	int was_min = 0;
 
 	counter = 0;
 	while (counter < 9)
 	{
+		/* check for negative int */
 		if (n < 0)
 		{
+			/* add one to min_int to avoid errors*/
 			if (n == min)
 			{
 				n += 1;
+				was_min = 1;
 			}
 			n *= -1;
 			_putchar('-');
 		}
 
+		/* avoids roblems when next digit it zero */
 		digit = (n / base_ten) % 10;
 
+		/* decrease base_ten until reaches first place */
 		if (digit == 0 && skip_zero)
 		{
 			base_ten /= 10;
@@ -39,5 +44,13 @@ void print_number(int n)
 		}
 		counter++;
 	}
-	_putchar(n % 10 + '0');
+	/* check if num was min_int and add 1 tp output accordingly */
+	if (was_min > 0)
+	{
+		_putchar(n % 10 + '1');
+	}
+	else
+	{
+		_putchar(n % 10 + '0');
+	}
 }
