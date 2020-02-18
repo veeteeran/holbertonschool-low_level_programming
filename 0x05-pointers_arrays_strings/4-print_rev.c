8d3cc00f07eba2d63a8dd14c1eac5b59ba6ebefc
Viet Tran <vietnamtran@gmail.com>
2020-02-18 03:20:41 +0000

    First commit with tested code

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * _strlen -returns the length of a string
+ * @s: the string to check
+ *
+ * Return: the length of a string as an int
+ */
+int _strlen(char *s)
+{
+	int counter;
+
+	counter = 0;
+	while (*(s + counter) != 0)
+	{
+		counter++;
+	}
+	return (counter);
+}
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: the string to print
+ */
+void print_rev(char *s)
+{
+	int length;
+
+	length = _strlen(s);
+	while (length >= 0)
+	{
+		_putchar(s[length]);
+		length--;
+	}
+	_putchar('\n');
+}
