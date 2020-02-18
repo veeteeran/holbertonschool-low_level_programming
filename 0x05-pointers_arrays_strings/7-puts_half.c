bc58ffd531b9fb9e10c4da02338b4c9db93b2ed3
Viet Tran <vietnamtran@gmail.com>
2020-02-18 16:42:56 +0000

    First commit with tested code

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: the string to check
+ *
+ * Return: the length of a string as an int
+ */
+int _strlen(char *s)
+{
+	int counter;
+
+	counter = 0;
+	while (*(s + counter) != 0)
+	{
+		counter++;
+	}
+	return (counter);
+}
+
+/**
+ * puts_half - prints half of a string then newline
+ * @str: the string to print
+ */
+void puts_half(char *str)
+{
+	int length, half;
+
+	length = _strlen(str);
+	half = length / 2;
+
+	if (half % 2 != 0)
+	{
+		while (half < length)
+		{
+			_putchar(str[half]);
+			half++;
+		}
+	}
+	_putchar('\n');
+}
