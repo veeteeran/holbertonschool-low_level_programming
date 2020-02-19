065f37ce2aa4d8e11b1c0a36e576152be1ec3372
Viet Tran <vietnamtran@gmail.com>
2020-02-19 01:53:47 +0000

    Remove trailing whitespace

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,13 +7,13 @@ void rev_string(char *s)
 {
 	int length, index, half, size, index2;
 	char temp;
-	
+
 	size = 0;
 	for (index = 0; s[index] != 0; index++)
 	{
 		size++;
 	}
-	
+
 	length = size - 1;
 	index2 = 0;
 	half = size / 2;
