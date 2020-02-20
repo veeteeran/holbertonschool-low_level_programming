ee1aee2d2d26f03e266a70fa4fa0f796fd29ecf4
Viet Tran <vietnamtran@gmail.com>
2020-02-20 15:51:08 +0000

    First commit with tested code

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,29 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
+ * _strcat - concatenates two strings
+ * @dest: string appended to
+ * @src: string appened to dest
  *
- *
- *
+ * Return: the concatenated string
  */
-void name(void)
+char *_strcat(char *dest, char *src)
 {
-	return (0);
+	int counter, dest_index;
+
+	dest_index = 0;
+	while (dest[dest_index] != 0)
+	{
+		dest_index++;
+	}
+
+	counter = 0;
+	while (dest[counter] != 0)
+	{
+		dest[dest_index] = src[counter];
+		dest_index++;
+		counter++;
+	}
+	dest[dest_index] = '\0';
+	return (dest);
 }
