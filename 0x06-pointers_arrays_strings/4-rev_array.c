ff4581e992303d1d6260843361a50b666a2964ef
Viet Tran <vietnamtran@gmail.com>
2020-02-20 21:43:36 +0000

    First commit with tested code

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,10 +1,24 @@
 #include "holberton.h"
 /**
+ * reverse_array - reverse an array of ints
+ * @a: the array to reverse
+ * @n: the number of array elements
  *
- *
- *
+ * Return: void
  */
-void name(void)
+void reverse_array(int *a, int n)
 {
-	return (0);
+	int start = 0;
+	int end = n - 1;
+	int temp;
+
+	while (start < n / 2)
+	{
+		temp = a[start];
+		a[start] = a[end];
+		a[end] = temp;
+
+		start++;
+		end--;
+	}
 }
