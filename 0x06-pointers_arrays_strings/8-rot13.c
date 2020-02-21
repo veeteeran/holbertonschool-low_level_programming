f513b90e6321c220ba4893eec82b256b027e82bd
Viet Tran <vietnamtran@gmail.com>
2020-02-21 02:25:18 +0000

    First commit with tested code

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,10 +1,30 @@
 #include "holberton.h"
 /**
+ * rot13 - encode a string using rot13
+ * @str: the string to encode
  *
- *
- *
+ * Return: the encoded string
  */
-void name(void)
+char *rot13(char *str)
 {
-	return (0);
+	int idx1 = 0;
+	int idx2;
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (str[idx1] != '\0')
+	{
+		idx2 = 0;
+		while (idx2 < 52)
+		{
+			if (str[idx1] == alpha[idx2])
+			{
+				str[idx1] = rot[idx2];
+				break;
+			}
+			idx2++;
+		}
+		idx1++;
+	}
+	return (str);
 }
