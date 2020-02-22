128ee0ca13ad07e7d53fc170f932651f49ed0877
Viet Tran <vietnamtran@gmail.com>
2020-02-22 23:47:45 +0000

    First commit with tested code

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+/**
+ * _atoi - convert a string to an int
+ * @s: the string to convert
+ *
+ * Return: an int representation of the string
+ */
+int _atoi(char *s)
+{
+	int digit = 0;
+	int b_10 = 1;
+	int num = 0;
+	int start, end, ndx, negative;
+
+	ndx = 0;
+	negative = 0;
+	while (s[ndx] && !(digit))
+	{
+		if (s[ndx] == '-')
+			negative++;
+		else if(s[ndx] >= '0' && s[ndx] <= '9')
+			digit = 1;
+		ndx++;
+	}
+	start = ndx - 1;
+	while (s[ndx] && digit)
+	{
+		if (s[ndx] < '0' || s[ndx] > '9')
+		{
+			digit = 0;
+			ndx--;
+		}
+		ndx++;
+	}
+	end = ndx - 1;
+	while (end >= start)
+	{
+		num += ((s[end] - '0') * b_10);
+		b_10 *= 10;
+		end--;
+	}
+	if (negative != 0 && negative % 2 != 0)
+		num *= -1;
+	return (0 + num);
+}
