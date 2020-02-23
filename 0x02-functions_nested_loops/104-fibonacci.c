fd3f30ac8ddebeb155a9647b23fe01c155ae31ef
Viet Tran <vietnamtran@gmail.com>
2020-02-23 15:34:15 +0000

    Added code to hold size of last three fib numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,28 +8,39 @@
  */
 int main(void)
 {
-	long double prev = 1;
-	long double curr = 2;
-	long double next;
-	int i;
+	int i, back_next = 0, back_prev = 1, back_curr = 2;
+
+	long double front_next = 0, front_prev = 0, front_curr = 0;
 
 	printf("1, 2, ");
 	for (i = 0; i < 96; i++)
 	{
-		next = prev + curr;
-		printf("%.Lf", next);
-
-		if (i != 95)
+		back_next = back_prev + back_curr;
+		front_next = front_prev + front_curr;
+		if (back_next > 99999999)
 		{
-			printf(", ");
+			back_next = back_next % 100000000;
+			front_next += 1;
 		}
+		if (front_next == 0)
+			printf("%d", back_next);
 		else
 		{
-			printf("\n");
+			printf("%.Lf", front_next);
+			if (back_next < 9999999)
+				printf("0%d", back_next);
+			else
+				printf("%d", back_next);
 		}
-
-		prev = curr;
-		curr = next;
+		if (i != 95)
+			printf(", ");
+		else
+			printf("\n");
+		back_prev = back_curr;
+		back_curr = back_next;
+		front_prev = front_curr;
+		front_curr = front_next;
 	}
 	return (0);
+
 }
