d0367df58ff13d53893054d1d834b749fc6a0ad6
Viet Tran <vietnamtran@gmail.com>
2020-02-24 18:34:35 +0000

    First commit with tested code

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,19 @@
 #include "holberton.h"
 /**
+ * _memset - fills memory with a constant byte
+ * @s: the string to fill with b
+ * @b: the char to fill s with
+ * @n: the number of bytes to fill s with b
  *
- *
- *
+ * Return: s filled with n bytes of b
  */
-void name(void)
+char *_memset(char *s, char b, unsigned int n)
 {
-	return (0);
+	unsigned int ndx = 0;
+
+	for (ndx = 0; ndx < n; ndx++)
+	{
+		s[ndx] = b;
+	}
+	return (s);
 }
