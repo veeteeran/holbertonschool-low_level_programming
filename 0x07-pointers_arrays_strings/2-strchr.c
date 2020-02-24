4579b16c291e997aaad44d75a26590a2250e0ad3
Viet Tran <vietnamtran@gmail.com>
2020-02-24 18:55:05 +0000

    Create a pointer to return first occurence of c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,18 +4,20 @@
  * @s: the string in which to find the character
  * @c: the character to look for in the string
  *
- * Return: pointer to first occurrence of the character, NULL if not found
+ * Return: pointer to first occurrence of the character, nul if not found
  */
 char *_strchr(char *s, char c)
 {
 	int ndx;
+	char *p;
 
 	for (ndx = 0; s[ndx] != '\0'; ndx++)
 	{
 		if (s[ndx] == c)
-		{
-			return (&s[ndx]);
+		{	
+			p = &s[ndx];
+			return (p);
 		}
 	}
-	return ("");
+	return ('\0');
 }
