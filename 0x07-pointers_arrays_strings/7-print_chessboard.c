05646bc7572075cb1e64e1a8eeb98bbc7fbe8d56
Viet Tran <vietnamtran@gmail.com>
2020-02-24 21:47:04 +0000

    First commit with tested code

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,10 +1,21 @@
 #include "holberton.h"
 /**
+ * print_chessboard - prints the chessboard
+ * @a: numbers of rows
  *
- *
- *
+ * Return: void
  */
-void name(void)
+void print_chessboard(char (*a)[8])
 {
-	return (0);
+	int rows, cols;
+
+	for (rows = 0; rows < 8; rows++)
+	{
+		for (cols = 0; cols < 8; cols++)
+		{
+			_putchar(a[rows][cols]);
+			if (cols == 7)
+				_putchar('\n');
+		}
+	}
 }
