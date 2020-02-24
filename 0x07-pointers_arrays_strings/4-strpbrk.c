ace5db54320387f422665c56df82b61eeb597553
Viet Tran <vietnamtran@gmail.com>
2020-02-24 20:33:37 +0000

    First commit with tested code

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,10 +1,22 @@
 #include "holberton.h"
 /**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: the string to compare
+ * @accept: the bytes to compare against
  *
- *
- *
+ * Return: pointer to byte in s that matches a byte in accept
  */
-void name(void)
+char *_strpbrk(char *s, char *accept)
 {
-	return (0);
+	int s_ndx, a_ndx;
+
+	for (s_ndx = 0; s[s_ndx] != '\0'; s_ndx++)
+	{
+		for (a_ndx = 0; accept[a_ndx] != '\0'; a_ndx++)
+		{
+			if (s[s_ndx] == accept[a_ndx])
+				return (s + s_ndx);
+		}
+	}
+	return ('\0');
 }
