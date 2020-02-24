19812270e8bdfc7849e91060b3c5c88e8221ca2e
Viet Tran <vietnamtran@gmail.com>
2020-02-24 18:34:57 +0000

    First commit with tested code

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,10 +1,19 @@
 #include "holberton.h"
 /**
+ * _memcpy - copies a memory area
+ * @dest: destnation string copied to
+ * @src: source string copied from
+ * @n: number of bytes to copy
  *
- *
- *
+ * Return: pointer to dest
  */
-void name(void)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	return (0);
+	unsigned int ndx;
+
+	for (ndx = 0; ndx < n; ndx++)
+	{
+		dest[ndx] = src[ndx];
+	}
+	return (dest);
 }
