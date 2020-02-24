e263ff3cfd234852fae00e0a21e0394e41f5fce2
Viet Tran <vietnamtran@gmail.com>
2020-02-24 22:52:49 +0000

    First commit with tested code

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,10 +1,40 @@
 #include "holberton.h"
 /**
+ * _strstr - locates a substring
+ * @haystack: the string to search
+ * @needle: the substring to search for in haystack
  *
- *
- *
+ * Return: pointer to beginning of the located substring
  */
-void name(void)
+char *_strstr(char *haystack, char *needle)
 {
-	return (0);
+	int h_ndx, n_ndx;
+
+	h_ndx = 0;
+	while (haystack[h_ndx] != '\0')
+	{
+		if (haystack[h_ndx] == needle[0])
+		{
+			h_ndx += 1;
+			n_ndx = 1;
+			while (needle[n_ndx] != '\0')
+			{
+				if (haystack[h_ndx] == needle[n_ndx])
+				{
+					h_ndx++;
+					n_ndx++;
+					if (needle[n_ndx + 1] == '\0')
+					{
+						return (&needle[0]);
+					}
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+		h_ndx++;
+	}
+	return ('\0');
 }
