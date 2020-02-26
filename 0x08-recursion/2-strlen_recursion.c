470cae0d531b2126b4311c61ec05ad023b83724c
Viet Tran <vietnamtran@gmail.com>
2020-02-26 19:14:29 +0000

    First commit with tested code

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,10 +1,14 @@
 #include "holberton.h"
 /**
+ * _strlen_recursion - return the length of a string
+ * @s: the tring whose length to return
  *
- *
- *
+ * Return: the length of the string as an int
  */
-void name(void)
+int _strlen_recursion(char *s)
 {
-	return (0);
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	else
+		return (0);
 }
