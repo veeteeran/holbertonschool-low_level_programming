54d228ea04b2d341be8be32d0e44407a9887aa1b
Viet Tran <vietnamtran@gmail.com>
2020-02-26 16:16:49 +0000

    First commit with tested code

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,17 @@
 #include "holberton.h"
 /**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: the string to print
  *
- *
- *
+ * Return: void
  */
-void name(void)
+void _puts_recursion(char *s)
 {
-	return (0);
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s++);
+		_puts_recursion(s);
+	}
 }
