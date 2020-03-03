6ee21d99195544323c48145c27621a712ef9017b
Viet Tran <vietnamtran@gmail.com>
2020-03-03 15:09:57 +0000

    First commit with tested code

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,10 +1,26 @@
-#include "holberton.h"
+#include <stdlib.h>
 /**
+ * create_array - creates an array of chars,
+ * and initializes it with a specific char
+ * @size: size of the array
+ * @c: the char to initialze in array
  *
- *
- *
+ * Return: pointer to the created array
  */
-void name(void)
+char *create_array(unsigned int size, char c)
 {
-	return (0);
+	char *ptr;
+	unsigned int ndx;
+
+	ptr = malloc(size * sizeof(char));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (ndx = 0; ndx < size; ndx++)
+	{
+		ptr[ndx] = c;
+	}
+	return (ptr);
 }
--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,10 +1,47 @@
-#include "holberton.h"
+#include "stdlib.h"
+int _strlen(char *s);
 /**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * contains a copy of the string given as a parameter
+ * @str: string to copy to newly allocated space
  *
- *
- *
+ * Return: ponter to allocated space
  */
-void name(void)
+char *_strdup(char *str)
 {
-	return (0);
+	char *ptr;
+	int len, ndx;
+
+	len = _strlen(str);
+
+	ptr = malloc(len * sizeof(char));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	ndx = 0;
+	while (str[ndx] != '\0')
+	{
+		ptr[ndx] = str[ndx];
+		ndx++;
+	}
+	return (ptr);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string to check
+ *
+ * Return: the length of a string as an int
+ */
+int _strlen(char *s)
+{
+	int counter;
+
+	for (counter = 0; s[counter] != '\0'; counter++)
+	{
+		continue;
+	}
+	return (counter);
 }
