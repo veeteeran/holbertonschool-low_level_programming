c75773bba734f35051cc669a94e5ee02016a1bb1
Viet Tran <vietnamtran@gmail.com>
2020-03-04 05:36:59 +0000

    Initial commit

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+/**
+ * strtow - return a pointer to an array of strings (words)
+ * @str: string to turn to words
+ *
+ * ReturnL pointer to array of strings
+ */
+char **strtow(__attribute__((unused)) char *str)
+{
+	
+	return (0);
+}
