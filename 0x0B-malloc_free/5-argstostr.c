9a3adfa1bffe7704deb4368cd15d8095a71a3335
Viet Tran <vietnamtran@gmail.com>
2020-03-04 04:06:35 +0000

    First commit with some code

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,10 +1,69 @@
 #include "holberton.h"
+#include <stdlib.h>
+int _strlen(char *s);
 /**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: argument count
+ * @av: argument vecotor
  *
- *
- *
+ * Return: pointer to the string
  */
-void name(void)
+char *argstostr(int ac, char **av)
 {
-	return (0);
+	char *ptr;
+	int ndx, jndx, len;
+
+	
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	len = ac;
+	for (ndx = 0; ndx < ac; ndx++)
+	{
+		len += _strlen(av[ndx]);
+	}
+
+	ptr = (char *) malloc(len * sizeof(char));
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	
+	for (ndx = 0; ndx < ac; ndx++)
+	{
+		for (jndx = 0; jndx < av[ndx]; jndx++)
+		{
+			ptr[ndx] = av[ndx][jndx];
+			if (ndx == ac -1)
+			{
+				ptr[ndx] = '\0';
+				ptr[ndx + 1] = '\n';
+			}
+			else
+			{
+				ptr[ndx] = '\n';
+			}
+		}
+	}
+	return (ptr);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string to check
+ *
+ * Return: the length of a string as an int
+ */
+int _strlen(char *s)
+{
+	int counter;
+
+	for (counter = 0; s[counter] != '\0'; counter++)
+	{
+		continue;
+	}
+	return (counter);
 }
