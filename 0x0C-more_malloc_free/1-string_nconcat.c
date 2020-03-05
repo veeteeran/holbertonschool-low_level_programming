4b17d1c082aa2e7e69ebd61855a9b9904e1a8757
Viet Tran <vietnamtran@gmail.com>
2020-03-05 18:37:40 +0000

    First commmit with tested code

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,10 +1,147 @@
 #include "holberton.h"
+#include <stdlib.h>
 /**
+ * string_nconcat - concatenates two strings
+ * @s1: string to copy
+ * @s2: string to concatenate
+ * @n: number of bytes of s to concatenate
  *
- *
- *
+ * Return: pointed to memory location of concatenated string
  */
-void name(void)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	return (0);
+	char *arr;
+	int s1_len;
+	unsigned int s2_len;
+
+	s1_len = _strlen(s1);
+	s2_len = _strlen(s2);
+
+	if (n <= s2_len)
+	{
+		arr = malloc(sizeof(*arr) * (s1_len + n + 1));
+		if (arr == NULL)
+		{
+			free(arr);
+			return (NULL);
+		}
+		_strcpy(arr, s1);
+		_strncat(arr, s2, n);
+		return (arr);
+	}
+	
+	arr = malloc(sizeof(*arr) * (s1_len + s2_len + 1));
+	if (arr == NULL)
+	{
+		free(arr);
+		return (NULL);
+	}
+	_strcpy(arr, s1);
+	_strcat(arr, s2);
+	return (arr);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string to check
+ *
+ * Return: the length of a string as an int
+ */
+int _strlen(char *s)
+{
+	int counter;
+
+	for (counter = 0; s[counter] != '\0'; counter++)
+	{
+		continue;
+	}
+	return (counter);
+}
+
+/**
+ * _strcpy - copy str and null byte from src to dest
+ * @dest: the copy of the string
+ * @src: the string to copy
+ *
+ * Return: the copied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int index;
+
+	for (index = 0; *(src + index) != 0; index++)
+	{
+		*(dest + index) = *(src + index);
+	}
+	*(dest + index) = '\0';
+
+	return (dest);
+}
+
+/**
+ * _strncat - concatenates two strings, at most n bytes from source
+ * @dest: string appended to
+ * @src: string to append to dest
+ * @n: number of bytes from src
+ *
+ * Return: concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int start = _strlen(dest);
+	int len_src = _strlen(src);
+	int index = 0;
+
+	if (len_src < n)
+	{
+		while (len_src > 0)
+		{
+			dest[start] = src[index];
+			start++;
+			index++;
+			len_src--;
+		}
+		dest[start] = '\0';
+	}
+	else
+	{
+		while (n > 0)
+		{
+			dest[start] = src[index];
+			start++;
+			index++;
+			n--;
+		}
+	}
+
+	return (dest);
+}
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: string appended to
+ * @src: string appened to dest
+ *
+ * Return: the concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int counter, dest_index;
+
+	dest_index = 0;
+	while (dest[dest_index] != 0)
+	{
+		dest_index++;
+	}
+
+	counter = 0;
+	/* does this need to be src or dest? */
+	while (src[counter] != 0)
+	{
+		dest[dest_index] = src[counter];
+		dest_index++;
+		counter++;
+	}
+	dest[dest_index] = '\0';
+	return (dest);
 }
