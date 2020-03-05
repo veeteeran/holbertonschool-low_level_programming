f8bf5d245ad0517b5807ec91295a4cd812a2d3c9
Viet Tran <vietnamtran@gmail.com>
2020-03-05 20:41:11 +0000

    Commit with updated header file

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,7 +10,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *arr;
-	int i;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -26,107 +26,3 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	return (arr);
 }
 
-/**
- * _strlen - returns the length of a string
- * @s: the string to check
- *
- * Return: the length of a string as an int
- */
-int _strlen(char *s)
-{
-	int counter;
-
-	for (counter = 0; s[counter] != '\0'; counter++)
-	{
-		continue;
-	}
-	return (counter);
-}
-
-/**
- * _strcpy - copy str and null byte from src to dest
- * @dest: the copy of the string
- * @src: the string to copy
- *
- * Return: the copied string
- */
-char *_strcpy(char *dest, char *src)
-{
-	int index;
-
-	for (index = 0; *(src + index) != 0; index++)
-	{
-		*(dest + index) = *(src + index);
-	}
-	*(dest + index) = '\0';
-
-	return (dest);
-}
-
-/**
- * _strncat - concatenates two strings, at most n bytes from source
- * @dest: string appended to
- * @src: string to append to dest
- * @n: number of bytes from src
- *
- * Return: concatenated string
- */
-char *_strncat(char *dest, char *src, int n)
-{
-	int start = _strlen(dest);
-	int len_src = _strlen(src);
-	int index = 0;
-
-	if (len_src < n)
-	{
-		while (len_src > 0)
-		{
-			dest[start] = src[index];
-			start++;
-			index++;
-			len_src--;
-		}
-		dest[start] = '\0';
-	}
-	else
-	{
-		while (n > 0)
-		{
-			dest[start] = src[index];
-			start++;
-			index++;
-			n--;
-		}
-	}
-
-	return (dest);
-}
-
-/**
- * _strcat - concatenates two strings
- * @dest: string appended to
- * @src: string appened to dest
- *
- * Return: the concatenated string
- */
-char *_strcat(char *dest, char *src)
-{
-	int counter, dest_index;
-
-	dest_index = 0;
-	while (dest[dest_index] != 0)
-	{
-		dest_index++;
-	}
-
-	counter = 0;
-	/* does this need to be src or dest? */
-	while (src[counter] != 0)
-	{
-		dest[dest_index] = src[counter];
-		dest_index++;
-		counter++;
-	}
-	dest[dest_index] = '\0';
-	return (dest);
-}
