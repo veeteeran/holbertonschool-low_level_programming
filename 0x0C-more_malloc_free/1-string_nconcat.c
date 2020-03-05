62b2a4bdc6754a72f2b5f719a3d220b199507d19
Viet Tran <vietnamtran@gmail.com>
2020-03-05 21:48:31 +0000

    Delete strcat func definition

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -110,32 +110,3 @@ char *_strncat(char *dest, char *src, int n)
 
 	return (dest);
 }
-
-/**
- * _strcat - concatenates two strings
- * @dest: string appended to
- * @src: string appened to dest
- *
- * Return: the concatenated string
- */
-char *_strcat(char *dest, char *src)
-{
-	int counter, dest_index;
-
-	dest_index = 0;
-	while (dest[dest_index] != 0)
-	{
-		dest_index++;
-	}
-
-	counter = 0;
-	/* does this need to be src or dest? */
-	while (src[counter] != 0)
-	{
-		dest[dest_index] = src[counter];
-		dest_index++;
-		counter++;
-	}
-	dest[dest_index] = '\0';
-	return (dest);
-}
