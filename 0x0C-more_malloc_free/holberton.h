9c6e352bf72e8d19112055be5b67172d54d9a2c6
Viet Tran <vietnamtran@gmail.com>
2020-03-05 20:28:19 +0000

    Update header file

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -13,5 +13,9 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
 
 #endif /* HOLBERTON_H */
