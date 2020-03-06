35ec4c92546fa3c84f52be4cd6b4cdf0da2f9f75
Viet Tran <vietnamtran@gmail.com>
2020-03-06 03:21:41 +0000

    Initial commit

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - multiplies two positive numbers
+ * @argc: argument count
+ * @argv: array of arguements
+ *
+ * Return: 0
+ */
+int main (argc, argv)
+{
+	void(argc);
+	void(argv);
+
+	exit(98);
+	printf("Error\n");
+	return (0);
+}
