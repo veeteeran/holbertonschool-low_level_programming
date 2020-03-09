088eaa6d30eece3bc46319abe9d61bd87c2ef0f0
Viet Tran <vietnamtran@gmail.com>
2020-03-09 19:53:12 +0000

    Add comments to struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - gathers information about a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ *
+ * Description: The struct contains basic information anout a dog
+ */ 
 struct dog
 {
 	char *name;
