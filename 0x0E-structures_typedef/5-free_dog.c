b341fff7cb8ed1e3133be24a41d609ab065b7416
Viet Tran <vietnamtran@gmail.com>
2020-03-10 02:10:26 +0000

    First commit with tested code

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees memory from dog struct
+ * @d: pointer to struct
+ *
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
