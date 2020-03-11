7099ff4f0c2e6316cf132ab191b404e02564f0ca
Viet Tran <vietnamtran@gmail.com>
2020-03-11 19:12:06 +0000

    Delete check for size <= 0 out of first condition

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,8 +11,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || size <= 0 || cmp == NULL)
+	if (array == NULL || cmp == NULL)
 		return (0);
+
 	if (size <= 0)
 		return (-1);
 
