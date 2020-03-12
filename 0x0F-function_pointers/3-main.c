04bc4e7508d55cf2e071da2a818745bc10fb6314
Viet Tran <vietnamtran@gmail.com>
2020-03-12 01:21:03 +0000

    Initial commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,12 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "3-calc.h"
 /**
  * main - Entry point
- *
- *
+ * @argc: the number of arguments
+ * @argv: array of arguments
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main(int argc, char **argv)
 {
+	int a, b;
+	int (*fptr)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	fptr = get_op_func(argv[2]);
+	if (fptr(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	else if ((!(strcmp(argv[2], "/")) || (!(strcmp(argv[2], "%")) && (!(strcmp(argv[3], "0"))))))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	else
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[3]);
+		printf("%d\n", fptr(argv[2])(a, b));
+
+	}
 	return (0);
 }
