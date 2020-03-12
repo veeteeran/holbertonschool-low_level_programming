26495b701fa9c9d47cf1705e7cea8eed754494e1
Viet Tran <vietnamtran@gmail.com>
2020-03-12 02:00:53 +0000

    Add more info to comment block

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,8 +1,10 @@
 #include "3-calc.h"
 /**
  * op_add - adds two ints
+ * @a: num 1
+ * @b: num 2
  *
- *
+ * Return: sum of a and b
  */
 int op_add(int a, int b)
 {
@@ -11,8 +13,10 @@ int op_add(int a, int b)
 
 /**
  * op_sub - subtracts two ints
+ * @a: num 1
+ * @b: num 2
  *
- *
+ * Return: difference of a and b
  */
 int op_sub(int a, int b)
 {
@@ -21,8 +25,10 @@ int op_sub(int a, int b)
 
 /**
  * op_mul - multiplies two ints
+ * @a: num 1
+ * @b: num 2
  *
- *
+ * Return: mult of a and b
  */
 int op_mul(int a, int b)
 {
@@ -31,8 +37,10 @@ int op_mul(int a, int b)
 
 /**
  * op_div - divides two ints
+ * @a: num 1
+ * @b: num 2
  *
- *
+ * Return: a divides b rounded down
  */
 int op_div(int a, int b)
 {
@@ -41,8 +49,10 @@ int op_div(int a, int b)
 
 /**
  * op_mod - gives remainder of two ints using mod operator
+ * @a: num 1
+ * @b: num 2
  *
- *
+ * Return: remainder of a/b
  */
 int op_mod(int a, int b)
 {
