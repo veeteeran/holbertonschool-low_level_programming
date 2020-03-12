f9bb93e240ef68e8bd7dd70f7811d699d1468acc
Viet Tran <vietnamtran@gmail.com>
2020-03-12 02:24:03 +0000

    Appease betty

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -20,7 +20,7 @@ int op_add(int a, int b)
  */
 int op_sub(int a, int b)
 {
-        return (a - b);
+	return (a - b);
 }
 
 /**
@@ -32,7 +32,7 @@ int op_sub(int a, int b)
  */
 int op_mul(int a, int b)
 {
-        return (a * b);
+	return (a * b);
 }
 
 /**
@@ -44,7 +44,7 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-        return (a / b);
+	return (a / b);
 }
 
 /**
@@ -56,5 +56,5 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-        return (a % b);
+	return (a % b);
 }
