cfaf877e0e70f57915c90611d7ae34ff4ea8f617
Viet Tran <vietnamtran@gmail.com>
2020-03-12 03:55:24 +0000

    Add conditions for correct arguments

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,9 +1,21 @@
+#include <stdio.h>
+#include <string.h>
 /**
  * main - Entry point
  *
  * Return: 0
  */
-int main(void)
+int main(int argc, char **argv)
 {
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
 	return (0);
 }
