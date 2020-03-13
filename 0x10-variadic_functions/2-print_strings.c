36079229604c0d608813c3c630839e40d1bbc912
Viet Tran <vietnamtran@gmail.com>
2020-03-13 20:54:30 +0000

    First commit with tested code, NULL check not complete

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,10 +1,29 @@
-#include "holberton.h"
+#include "variadic_functions.h"
 /**
+ * print_strings - prints strings, followed by a new line
+ * @separator: string to be printed between the strings
+ * @n: the number of strings passed to the function
+ * @...: variable arguments
  *
- *
- *
+ * Return: nothing
  */
-void name(void)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
-	return (0);
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		if (va_arg(args, char *) == NULL)
+			printf("nil");
+		else
+			printf("%s", va_arg(args, char *));	
+
+		if (i == n - 1)
+			printf("\n");
+		else if (separator != NULL)
+			printf("%s", separator);
+	}
+	va_end(args);
 }
