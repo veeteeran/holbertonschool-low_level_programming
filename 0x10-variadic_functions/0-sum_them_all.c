62d1cd704b07ec6fffa557d089e932dbc89c7a01
Viet Tran <vietnamtran@gmail.com>
2020-03-13 13:56:56 +0000

    First commit with tested code

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,10 +1,22 @@
-#include "holberton.h"
+#include "variadic_functions.h"
 /**
+ * sum_them_all - returns the sum of all its parameters
+ * @n: number of variable args
+ * @...: variadic arguments
  *
- *
- *
+ * Return: sum of all arguments
  */
-void name(void)
+int sum_them_all(const unsigned int n, ...)
 {
-	return (0);
+	va_list args;
+	unsigned int i;
+	int sum = 0;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+	return (sum);
 }
