6362f9fa16461930b8898c3943a0766768418043
Viet Tran <vietnamtran@gmail.com>
2020-03-13 20:16:17 +0000

    First commit with tested code

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,10 +1,26 @@
-#include "holberton.h"
+#include "variadic_functions.h"
+#include <stdio.h>
 /**
+ * print_numbers - prints numbers, followed by a new line
+ * @separator: string to be printer between numbers
+ * @n: number of args
  *
- *
- *
+ * Return: nothing
  */
-void name(void)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	return (0);
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+
+		if (i == n - 1)
+			printf("\n");
+		else if (separator != NULL)
+			printf("%s", separator);
+	}
+	va_end(args);
 }
