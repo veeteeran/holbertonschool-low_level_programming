db941dcfc041e4803867e1658fc5386e070a1b47
Viet Tran <vietnamtran@gmail.com>
2020-03-14 17:09:39 +0000

    Delete trailing whitespace, line 23

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (ptr == NULL)
 			printf("(nil)");
 		else
-			printf("%s", ptr); 
+			printf("%s", ptr);
 
 		if (i == n - 1)
 			printf("\n");
