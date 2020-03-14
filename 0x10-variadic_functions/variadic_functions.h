7cecb83d6ac7b0588a287f47fed754389ffd2de7
Viet Tran <vietnamtran@gmail.com>
2020-03-14 17:01:40 +0000

    Add _putchar prototype

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -16,6 +16,7 @@ typedef struct format
 	void (*fptr)(va_list);
 } fmt_t;
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
