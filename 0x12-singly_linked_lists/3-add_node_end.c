a8c59bbeb42de82d689a46dc07c6dd4196345708
Viet Tran <vietnamtran@gmail.com>
2020-03-19 22:37:18 +0000

    Some bullshit code

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,25 +1,44 @@
-#ifndef LISTS_H
-#define LISTS_H
+#include "lists.h"
+int _strlen(const char *s);
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: pointer to pointer of struct
+ * @str: data to enter for first member of struct
+ *
+ * Return: the address of the new element at end of linked list
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new = (list_t *)malloc(sizeof(list_t));
+	list_t *temp = *head;
+
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = _strlen(str);
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+
+	while (temp)
+		temp = temp->next;
+	temp = new;
+	return (new);
+}
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
+ * _strlen - function that returs the length of a string
+ *@s: string input by a user
+ * Return: (void)
  */
-typedef struct list_s
+int _strlen(const char *s)
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
+	int len;
 
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-#endif /* LISTS_H */
+	for (len = 0; s[len] != '\0'; len++)
+		;
+
+	return (len);
+}
