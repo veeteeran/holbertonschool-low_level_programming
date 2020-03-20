08f41c031954bfddd44f0675e7d79314919e9cf0
Viet Tran <vietnamtran@gmail.com>
2020-03-20 01:02:50 +0000

    First commit with tested code

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,25 +1,22 @@
-#ifndef LISTS_H
-#define LISTS_H
-
+#include "lists.h"
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
+ * free_list - frees a list_t list
+ * @head: pointer to head of list
  *
- * Description: singly linked list node structure
- * for Holberton project
+ * Return: void
  */
-typedef struct list_s
+void free_list(list_t *head)
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
+	list_t *temp;
 
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-#endif /* LISTS_H */
+	temp = head;
+	while (temp)
+	{
+		temp = temp->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+	free(temp);
+	free(head);
+}
