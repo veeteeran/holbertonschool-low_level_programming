7f3507f269eaa1f185d0115be89822afe11ec4eb
Viet Tran <vietnamtran@gmail.com>
2020-03-23 15:52:34 +0000

    First commit with tested code

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,10 +1,21 @@
 #include "lists.h"
 /**
+ * free_listint2 - frees a listint_t list, sets the head to NULL
+ * @head: pointer to pointer of first node
  *
- *
- *
+ * Return: void
  */
-void name(void)
+void free_listint2(listint_t **head)
 {
-	return (0);
+	listint_t *temp;
+
+	temp = *head;
+	while (temp)
+	{
+		temp = temp->next;
+		free(*head);
+		*head = temp;
+	}
+
+	*head = NULL;
 }
