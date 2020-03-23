f05964586cdf86cc827c9dc447360a1842eb2b79
Viet Tran <vietnamtran@gmail.com>
2020-03-23 22:37:48 +0000

    First commit with tested code

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,10 +1,37 @@
 #include "lists.h"
 /**
+ * delete_nodeint_at_index - deletes the node at index of linked list
+ * @head: pointer to pointer of first node
+ * @index: index of node to delete
  *
- *
- *
+ * Return: 1 on success, -1 otherwise
  */
-void name(void)
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	return (0);
+	listint_t *temp;
+	listint_t *curr;
+
+	if (*head == NULL)
+		return (-1);
+
+	temp = *head;
+	if (index == 0)
+	{
+		*head = temp->next;
+		free(temp);
+		return(1);
+	}
+	while (index > 1)
+	{
+		temp = temp->next;
+		if(temp == NULL || temp->next == NULL)
+			return (-1);
+
+		index--;
+	}
+
+	curr = temp->next->next;
+	free(temp->next);
+	temp->next = curr;
+	return (1);
 }
