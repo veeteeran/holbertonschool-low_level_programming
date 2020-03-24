1c12c5a70d14cbb8df4fb684bee29192893e4582
Viet Tran <vietnamtran@gmail.com>
2020-03-24 13:51:46 +0000

    Refactor NULL checks

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,10 +10,10 @@ size_t print_listint(const listint_t *h)
 	size_t node_count;
 
 	if (h == NULL)
-		return (NULL);
+		return (0);
 	while (h)
 	{
-		if (h->n == NULL)
+		if (h->n == 0)
 			printf("0");
 		else
 			printf("%d\n", h->n);
