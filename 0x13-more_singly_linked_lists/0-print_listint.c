cf113b10ee6c39316d64765f307f493cb4eb31cc
Viet Tran <vietnamtran@gmail.com>
2020-03-24 13:55:39 +0000

    Initialize node_count

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,16 +7,11 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t node_count;
+	size_t node_count = 0;
 
-	if (h == NULL)
-		return (0);
 	while (h)
 	{
-		if (h->n == 0)
-			printf("0");
-		else
-			printf("%d\n", h->n);
+		printf("%d\n", h->n);
 		node_count++;
 		h = h->next;
 	}
