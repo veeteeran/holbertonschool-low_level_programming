14e0356dd09ad98db1e96859bf2938db65d51672
Viet Tran <vietnamtran@gmail.com>
2020-03-24 15:12:45 +0000

    Check head for NULL before malloc for new

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,8 +12,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp;
 	listint_t *new;
 
+	if (head == NULL)
+		return (NULL);
+
 	new = malloc(sizeof(listint_t));
-	if (new == NULL || head == NULL)
+	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
