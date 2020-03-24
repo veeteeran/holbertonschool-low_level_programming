aaf5cce582ba70a2d42149e4e5be422a5502459f
Viet Tran <vietnamtran@gmail.com>
2020-03-24 14:22:28 +0000

    head not *head = NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,13 +9,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	temp = *head;
-	while (temp)
+	while (*head)
 	{
-		temp = temp->next;
-		free(*head);
-		*head = temp;
+		temp = *head;
+		*head = temp->next;
+		free(temp);
 	}
-
+	
 	*head = NULL;
 }
