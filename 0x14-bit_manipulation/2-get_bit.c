e0ea75b28dafa68eab4d2f7aabadf64ca768f0fb
Viet Tran <vietnamtran@gmail.com>
2020-03-26 21:08:39 +0000

    Add check fro 64 bit machinne, store bit shifted value in new var

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,13 +10,14 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int count;
+	unsigned long int bit;
 
-	if (index >  sizeof(unsigned long int))
+	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
 
 	for (count = 0; count < index; count++)
 	{
-		n = (n >> 1);
+		bit = (n >> 1);
 	}
-	return (n & 1);
+	return (bit & 1);
 }
