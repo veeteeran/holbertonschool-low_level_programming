847e42fbb78e71fdea5126086734545abd05771b
Viet Tran <vietnamtran@gmail.com>
2020-03-26 20:53:19 +0000

    First commit with tested code

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,11 +1,19 @@
 #include "holberton.h"
 
 /**
- * get_bit -
+ * get_bit - returns the value of a bit at a given index
+ * @n: the number to work with
+ * @index: index of the bit
  *
- * Return: 
+ * Return: value of bit at index or -1 on error
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned int count;
 
+	for (count = 0; count < index; count++)
+	{
+		n = (n >> 1);
+	}
+	return (n & 1);
 }
