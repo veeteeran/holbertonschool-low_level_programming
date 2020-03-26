c4de961663e5ae0c24e1e132f56179968c155792
Viet Tran <vietnamtran@gmail.com>
2020-03-26 21:11:15 +0000

    Shift by index given, no loop necessary

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,15 +9,11 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int count;
 	unsigned long int bit;
 
 	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
 
-	for (count = 0; count < index; count++)
-	{
-		bit = (n >> 1);
-	}
+	bit = (n >> index);
 	return (bit & 1);
 }
