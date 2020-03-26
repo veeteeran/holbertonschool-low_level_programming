ae173b338c506f59a591170a91a0d84e76b5acee
Viet Tran <vietnamtran@gmail.com>
2020-03-26 23:36:24 +0000

    First commit with tested code

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,11 +1,22 @@
 #include "holberton.h"
 
 /**
- * flip_bits -
+ * flip_bits - flip bits to get from one number to another
+ * @n: the number to flip
+ * @m: flip to this number
  *
- * Return: 
+ * Return: number of bits to flip
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned long int mask;
+	unsigned int sum = 0;
 
+	mask = n ^ m;
+	while (mask > 0)
+	{
+		sum += mask & 1;
+		mask >>= 1;
+	}
+	return (sum);
 }
