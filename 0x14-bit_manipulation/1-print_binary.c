48e45d86eae5848a182e5ded6d131e105f9281f5
Viet Tran <vietnamtran@gmail.com>
2020-03-26 20:41:27 +0000

    Use bitwise shift with recursion, magic

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,54 +8,8 @@ unsigned long int _pow_recursion(unsigned long int x, int y);
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int bin_place;
-	int exponent = 0;
+	if (n > 1)
+		print_binary(n>>1);
 
-	if (n == 0)
-		_putchar('0');
-
-	bin_place = _pow_recursion(2, exponent);
-	while (bin_place <= n)
-	{
-		exponent++;
-		bin_place = _pow_recursion(2, exponent);
-	}
-
-	exponent--;
-	bin_place = _pow_recursion(2, exponent);
-	while (exponent >= 0)
-	{
-		if (bin_place > n)
-			_putchar('0');
-		else
-		{
-			_putchar('1');
-			n -= bin_place;
-		}
-		exponent--;
-		bin_place = _pow_recursion(2, exponent);
-	}
-}
-
-/**
- * _pow_recursion - return the value of x to power of y
- * @x: the number to raise
- * @y: power to raise x to
- *
- * Return: x raised to y
- */
-unsigned long int _pow_recursion(unsigned long int x, int y)
-{
-	if (y > 0)
-	{
-		return (x * _pow_recursion(x, y - 1));
-	}
-	else if (y == 0)
-	{
-		return (1);
-	}
-	else
-	{
-		return (-1);
-	}
+	_putchar((n & 1) + '0');
 }
