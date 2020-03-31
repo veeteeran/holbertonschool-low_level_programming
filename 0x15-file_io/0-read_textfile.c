6062e452f63bad3c3098a769de98676b0d68c8d7
Viet Tran <vietnamtran@gmail.com>
2020-03-31 17:10:16 +0000

    First commit with tested code

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,11 +1,30 @@
 #include "holberton.h"
 
 /**
- * read_textfile -
+ * read_textfile - read text file, prints it POSIX standard output
  *
- * Return: 
+ * Return: num of letters read and printed, 0 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd, num_bytes;
+	char *buf;
 
+	if (filename == NULL)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters + 1));
+	if (buf == NULL)
+		return ('\0');
+
+	fd = open(filename, O_RDONLY, 400);
+	if (fd == -1)
+		return (0);
+
+	read(fd, buf, letters);
+	buf[letters + 1] = '\0';
+	close(fd);
+
+	num_bytes = write(STDOUT_FILENO, buf, letters + 1);
+	return(num_bytes);
 }
