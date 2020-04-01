7ffc249d6bfc02a1447b8434750358f57393458d
Viet Tran <vietnamtran@gmail.com>
2020-04-01 01:53:30 +0000

    Add err message functions, account for more than 1024

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
-int _strlen(char *str);
-#define RWRWR (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+void read_err(char *arg);
+void write_err(char *arg);
+void close_err(int arg);
 /**
  * main - the entry point
  * @argc: number of arguments
@@ -10,11 +11,8 @@ int _strlen(char *str);
  */
 int main(int argc, char *argv[])
 {
-	int op_to, op_from, rd, wr, cl_to, cl_from, len;
+	int op_to, op_from, rd, wr;
 	char argc_err[] = "Usage: cp file_from file_to";
-	char argv1_err[] = "Error: Can't read from file";
-	char wr_err[] = "Error: Can't write to";
-	char cl_err[] = "Error: Can't close fd";
 	char *buf;
 
 	if (argc != 3)
@@ -22,57 +20,85 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "%s\n", argc_err);
 		exit(97);
 	}
-	/* 664 permission, leave unchaged if already exists*/
-	op_to = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+	op_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (op_to == -1)
-		return (-1);
+		read_err(argv[2]);
 
-	len = _strlen(argv[1]);
-	buf = malloc(sizeof(char) * len);
+	buf = malloc(sizeof(char) * 1024);
 	if (buf == NULL)
+		/* read_err? */
 		return ('\0');
+
 	op_from = open(argv[1], O_RDONLY);
+	if (op_from == -1)
+		read_err(argv[1]);
+
 	rd = read(op_from, buf, 1024);
+	if (rd == -1)
+		read_err(argv[1]);
+
 	wr = write(op_to, buf, rd);
-	if (op_from == -1 || rd == -1)
-	{
-		dprintf(STDERR_FILENO, "%s %s\n", argv1_err, argv[1]);
-		exit(98);
-	}
-	if (op_to == -1 || wr == -1)
-	{
-		dprintf(STDERR_FILENO, "%s %s\n", wr_err, argv[2]);
-		exit(99);
-	}
+	if (wr == -1)
+		write_err(argv[2]);
 
-	cl_to = close(op_to);
-	cl_from = close(op_from);
-	if (cl_to == -1 || cl_from == -1)
+	while (rd == 1024)
 	{
-		dprintf(STDERR_FILENO, "%s %d\n", cl_err, op_to);
-		exit(100);
+		rd = read(op_from, buf, 1024);
+		if (rd == -1)
+			read_err(argv[1]);
+		wr = write(op_to, buf, rd);
+		if (wr == -1)
+			write_err(argv[2]);
 	}
-	if (cl_from == -1)
-	{
-		dprintf(STDERR_FILENO, "%s %d\n", cl_err, op_from);
-		exit(100);
-	}
-
 	free(buf);
+
+	if (close(op_to) == -1)
+		close_err(op_to);
+
+	if (close(op_from) == -1)
+		close_err(op_from);
+
 	return (1);
 }
 
 /**
- * _strlen - find length of given string
- * @str: the string whose length to find
+ * read_err - prints read error message
+ * @arg: arg of read error
  *
- * Return: length of a string
+ * Return: void
  */
-int _strlen(char *str)
+void read_err(char *arg)
 {
-	int count;
+	char argv1_err[] = "Error: Can't read from file";
 
-	for (count = 0; *str++; count++)
-		;
-	return (count);
+	dprintf(STDERR_FILENO, "%s %s\n", argv1_err, arg);
+	exit(98);
+}
+
+/**
+ * write_err - prints write error message
+ * @arg: arg of write error
+ *
+ * Return: void
+ */
+void write_err(char *arg)
+{
+	char wr_err[] = "Error: Can't write to";
+
+	dprintf(STDERR_FILENO, "%s %s\n", wr_err, arg);
+	exit(99);
+}
+
+/**
+ * close_err - prints close error message
+ * @arg: arg of close error
+ *
+ * Return: void
+ */
+void close_err(int arg)
+{
+	char cl_err[] = "Error: Can't close fd";
+
+	dprintf(STDERR_FILENO, "%s %d\n", cl_err, arg);
+	exit(100);
 }
