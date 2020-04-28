a11d844f01d2e3e3149561f1c9ee9999b3a8d7bc
Viet Tran <vietnamtran@gmail.com>
2020-04-28 22:50:48 +0000

    Fix indentation

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     new_str = ""
     for i in range(len(str)):
         if ord(str[i]) >= ord('a') and ord(str[i]) <= ord('z'):
-           new_str = new_str + chr((ord(str[i]) - 32))
+            new_str = new_str + chr((ord(str[i]) - 32))
         else:
             new_str = new_str + str[i]
 
