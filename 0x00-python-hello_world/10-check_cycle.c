10a7c2aaec1c1738a6fc2d4d4063f007346e708a
Viet Tran <vietnamtran@gmail.com>
2020-04-28 02:43:20 +0000

    Initial commit

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: the list to check
+ *
+ * Return: 0, if no cycle, 1 if there is
+ */
+int check_cycle(listint_t *list)
+{
+	return (0);
+}
