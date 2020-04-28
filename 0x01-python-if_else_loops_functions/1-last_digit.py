4e8c87b6e7014d4b5d69cbd0245f58a210ccd1fe
Viet Tran <vietnamtran@gmail.com>
2020-04-28 17:45:11 +0000

    First commit with tested code

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-def uppercase(str):
+import random
+number = random.randint(-10000, 10000)
+last_digit = number % 10
+print("Last digit of {} is {} ".format(number, last_digit), end='') 
+if last_digit > 5:
+    print("and is greater that 5")
+elif last_digit == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
