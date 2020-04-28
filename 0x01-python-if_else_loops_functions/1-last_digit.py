b202bffac821de3e95e839282e77b24d28e75fba
Viet Tran <vietnamtran@gmail.com>
2020-04-28 18:34:18 +0000

    Delete colon

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -17,7 +17,7 @@ if was_neg:
 print("Last digit of {:d} is {:d} ".format(number, last_digit), end='')
 
 if last_digit > 5:
-    print("and is greater than: 5")
+    print("and is greater than 5")
 elif last_digit == 0:
     print("and is 0")
 elif last_digit < 6 and last_digit != 0:
