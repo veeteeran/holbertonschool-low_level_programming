a8eae135a6d5bfb0f52764b6afc61f356ed4e482
Viet Tran <vietnamtran@gmail.com>
2020-04-28 17:48:32 +0000

    Write last elif instead of else

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,5 +7,5 @@ if last_digit > 5:
     print("and is greater that 5")
 elif last_digit == 0:
     print("and is 0")
-else:
+elif last_digit < 6 and last_digit != 0:
     print("and is less than 6 and not 0")
