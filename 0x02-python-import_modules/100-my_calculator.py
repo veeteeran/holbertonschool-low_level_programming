f150d3ce434ef9cb60a4cbd88e42b0e4d6e9fef6
Viet Tran <vietnamtran@gmail.com>
2020-04-30 18:34:42 +0000

    First commit with tested code

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+
+    length = len(argv)
+    if length != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit (1)
+    elif argv[2] != '+' and argv[2] != '-' and argv[2] != '*' and argv[2] != '/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit (1)
+    else:
+        a = int(argv[1])
+        b = int(argv[3])
+
+        if argv[2] == '+':
+            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], add(a, b)))
+        elif argv[2] == '-':
+            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], sub(a, b)))
+        elif argv[2] == '*':
+            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], mul(a, b)))
+        elif argv[2] == '/':
+            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], div(a, b)))
