f37acc851aecc3356044881d1249bc69b2ec9550
Viet Tran <vietnamtran@gmail.com>
2020-04-30 03:07:30 +0000

    First commit with tested code

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,5 +10,16 @@
  */
 int check_cycle(listint_t *list)
 {
+	listint_t *tortoise = list;
+	listint_t *hare = list;
+
+	while (tortoise && hare && hare->next)
+	{
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+
+		if (tortoise == hare)
+			return (1);
+	}
 	return (0);
 }
