c9b9fabf7bb0bfd79a6117e8cd7baed26cbcf2df
Viet Tran <vietnamtran@gmail.com>
2020-05-01 12:54:40 +0000

    First commit with tested code

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,11 +1,22 @@
 #include "lists.h"
 
 /**
- * dlistint_len -
+ * dlistint_len - counts number of elements in list
+ * @h: pointer to head of list
  *
- * Return: 
+ * Return: number of elements in list
  */
 size_t dlistint_len(const dlistint_t *h)
 {
+	const dlistint_t *current;
+	size_t num_nodes = 0;
 
+	current = h;
+	while (current)
+	{
+		num_nodes++;
+		current = current->next;
+	}
+
+	return (num_nodes);
 }
