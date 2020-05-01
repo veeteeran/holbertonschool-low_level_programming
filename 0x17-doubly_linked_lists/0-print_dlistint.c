80ca51f9a4c0caf264516926e8a184f634da3537
Viet Tran <vietnamtran@gmail.com>
2020-05-01 12:50:47 +0000

    First commit with tested code

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,5 +7,16 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
+	size_t num_nodes = 0;
+	const dlistint_t *current;
 
+	current = h;
+	while (current)
+	{
+		printf("%d\n", current->n);
+		num_nodes++;
+		current = current->next;
+	}
+
+	return (num_nodes);
 }
