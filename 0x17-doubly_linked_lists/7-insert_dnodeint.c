8db1170e575b2e6cfefe3e53e09b945adab70da8
Viet Tran <vietnamtran@gmail.com>
2020-05-01 16:06:08 +0000

    Handle when head is null

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,9 +14,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp;
 	unsigned int node_num;
 
-	if (h == NULL)
-		return (NULL);
-
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
@@ -25,21 +22,21 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->prev = NULL;
 	new->next = NULL;
 
-	if (idx == 0)
+	if (h == NULL)
+		return (new);
+	else if (idx == 0)
 	{
 		new->next = *h;
 		*h = new;
-		return (new);
 	}
-
-	temp = *h;
-	for (node_num = 0; node_num < idx; node_num++)
+	else if (*h == NULL)
+		*h = new;
+	else
 	{
-		if (temp == NULL)
-			return (NULL);
-		temp = temp->next;
+		temp = *h;
+		for (node_num = 0; node_num < idx; node_num++)
+			temp = temp->next;
 	}
-
 	new->next = temp->next;
 	new->prev = temp;
 	temp->next = new;
