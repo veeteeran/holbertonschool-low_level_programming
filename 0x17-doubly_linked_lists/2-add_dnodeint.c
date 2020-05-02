ecb5a9ef408aa5dd3221a1fa90a3ad40ea152b02
Viet Tran <vietnamtran@gmail.com>
2020-05-02 21:19:23 +0000

    Check for empty list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,9 +16,17 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new->n = n;
 	new->prev = NULL;
-	new->next = *head;
-	*head->prev = new;
-	*head = new;
+	if (*head == NULL)
+	{
+		*head = new;
+		new->next = NULL;
+	}
+	else
+	{
+		new->next = *head;
+		(*head)->prev = new;
+		*head = new;
+	}
 
 	return (*head);
 }
