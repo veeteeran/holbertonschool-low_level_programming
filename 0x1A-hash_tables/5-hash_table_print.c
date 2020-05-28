94c4179d2cacca4ccd19f37191edce82333e18d6
Viet Tran <vietnamtran@gmail.com>
2020-05-28 21:19:09 +0000

    First commit with tested code

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,11 +1,28 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print -
+ * hash_table_print - prints a hash table
+ * @ht: the hash table
  *
- * Return: 
+ * Return: nothing
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	unsigned long int index;
+	int flag = 0;
 
+	if (ht == NULL)
+		printf("{}\n");
+	else
+		printf("{");
+		for (index = 0; index < ht->size; index++)
+		{
+			if (ht->array[index] == NULL)
+				continue;
+			if (flag == 1)
+				printf(", ");
+			printf("'%s': '%s'", ht->array[index]->key, ht->array[index]->value);
+			flag = 1;
+		}
+		printf("}\n");
 }
