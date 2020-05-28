3ee135e124bbe2fbcff8516a71d7cfa993fd5213
Viet Tran <vietnamtran@gmail.com>
2020-05-28 17:57:26 +0000

    First commit with tested code

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,11 +1,26 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create -
+ * hash_table_create - creates a hash table
+ * @size: size of the array
  *
- * Return: 
+ * Return: pointer to newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *new;
 
+	new = malloc(sizeof(hash_table_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->size = size;
+	new->array = malloc(sizeof(hash_node_t *) * size);
+	if (new->array == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	return (new);
 }
