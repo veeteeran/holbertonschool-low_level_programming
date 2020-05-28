b86da7bfe7d72f1500a6132f98fda156ec4708ba
Viet Tran <vietnamtran@gmail.com>
2020-05-28 20:53:06 +0000

    First commit with tested code, add string.h

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,11 +1,24 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get -
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table to look into
+ * @key: key to look for
  *
- * Return: 
+ * Return: the value associated with the element, NULL if key not found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int index;
+	char *value;
 
+	if (ht == NULL)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[index] == NULL)
+		return (NULL);
+
+	value = ht->array[index]->value;
+	return (value);
 }
