638a8290861ada77e5d62bcc2bc97776954787ab
Viet Tran <vietnamtran@gmail.com>
2020-05-28 18:13:29 +0000

    FIrst commit with tested code

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,11 +1,15 @@
 #include "hash_tables.h"
 
 /**
- * key_index -
+ * key_index - gives the index of a k
+ * @key: the key
+ * @size: size of the array of the hash table
  *
- * Return: 
+ * Return: index where key/value pair should be stored
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
+	unsigned long int hash = hash_djb2(key);
 
+	return (hash % size);
 }
