7a1d3bffc9e8984e6ea3f8df6d544b962299b552
Viet Tran <vietnamtran@gmail.com>
2020-08-25 15:21:43 +0000

    First commit with tested code

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,9 +9,18 @@
  *
  * Return: the first index where value is located,
  * or -1 if value is not present in array or if array is NULL
- /
+ */
 
 int linear_search(int *array, size_t size, int value)
 {
+	int i;
 
+	for (i = 0; i < (int)size; i++)
+	{
+		printf("Value checked array [%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
 }
